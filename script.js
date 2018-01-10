6c6e8f74941691c090c948798295d7783eee8c18
mounika <mounikac.58@gmail.com>
2018-01-09 18:15:02 -0600

    Fourth Commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var score = 0;
-var gscore = 0;
+var score = 0, gscore = 0, ghost=false;
 var player = {
     x: 50,
     y: 50,
@@ -7,12 +6,21 @@ var player = {
     pacdir: 0,
     psize: 32,
     speed: 5
-}
+};
 var enemy = {
     x: 150,
     y: 250,
-    speed: 5
-}
+    speed: 5,
+    moving: 0,
+    dirx: 0,
+    diry: 0
+};
+var powerdot = {
+    x: 10,
+    y: 10,
+    powerup: false
+};
+
 
 
 
@@ -62,17 +70,48 @@ function playgame(){
 }
 
 function myNum(n){
-    return Math.floor(Math.random() *n) +1;
+    return Math.floor(Math.random()*n);
 }
  function render() {
     context.fillStyle = "#5a82c4";
-    context.fillRect(0,0,canvas.width,canvas.height);
+    context.fillRect(0,0,canvas.width, canvas.height);
+
+    if(!ghost){
+        enemy.ghostNum = myNum(5)* 64;
+        enemy.x = myNum(450);
+        enemy.y = myNum(250)+30;
+        ghost = true;
+    }
+    if(enemy.moving <0){
+        enemy.moving = (myNum(20)*3)+myNum(1);
+        enemy.speed = myNum(3)+1;
+        enemy.dirx = 0;
+        enemy.diry = 0;
+        if(enemy.moving % 2){
+            if(player.x < enemy.x){enemy.dirx = -enemy.speed;} else {enemy.dirx = enemy.speed;}
+        }else{
+            if(player.y < enemy.y){enemy.diry = -enemy.speed;} else {enemy.diry = enemy.speed;}
+        }
+    }
+     enemy.moving--;
+     enemy.x = enemy.x + enemy.dirx;
+     enemy.y = enemy.y + enemy.diry;
+
+
+     if(enemy.x >= (canvas.width-32)) {enemy.x=0;}
+     if(enemy.y >= (canvas.height-32)) {enemy.y=0;}
+     if(enemy.x < 0) {enemy.x=(canvas.width-32);}
+     if(enemy.y < 0) {enemy.y=(canvas.height-32);}
+
+
+
+
      context.font = "20px Verdana";
      context.fillStyle = "white";
      context.fillText("Pac-Man: " +score+" vs Ghost:" +gscore, 50, 20);
 
 
-     context.drawImage(mainImage, 0, 0, 32, 32, enemy.x, enemy.y, 32,32);
+     context.drawImage(mainImage, enemy.ghostNum,0, 32, 32, enemy.x, enemy.y, 32,32);
      context.drawImage(mainImage, player.pacmouth, player.pacdir, 32, 32, player.x, player.y, 32,32);
 
 
